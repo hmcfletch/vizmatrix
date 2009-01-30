d050ff74a52695a2d60210010a8d1453d33b940f
Les Fletcher <les@HMCFletch.local>
2009-01-29 20:43:38 -0800

    added some crosshairs

--- prototype/VizMatrixHTMLGen.rb
+++ prototype/VizMatrixHTMLGen.rb
@@ -1,16 +1,17 @@
-filename = "orig.html"
+filename = "HTMLbase.html"
+jsdatafile = "data.js"
 
-table = ""
-
-x = 800
-y = 600
-
+# BEGIN CONFIGURATION VARIABLES #
+x = 200
+y = 200
 cellsize = 1
-
-threshold = 0.1
+threshold = 0.01
+# END CONFIGUREATION VARIABLES #
 
 file = File.new(filename,"r")
 html = ""
+data = []
+
 while (line = file.gets):
     html += line
 end
@@ -20,24 +21,28 @@ j = 0
 
 while ( i < x ):
     j = 0
-    table << "  <div class=\"line\">"
     while ( j < y ):
-        style = "white"
-        value = 0
         if (rand(0) < threshold ):
-            style = "blue"
             value = rand(1000);
+            data << { 'i' => i, 'j' => j, 'val' => value }
         end
-        
-        table << ('<div class="' + style + "\" value=\"" + value.to_s + "\" i=\"" + i.to_s + "\" j=\"" + j.to_s + "\">&nbsp;</div>")
         j += 1
     end
-    table << "</div>"
     i += 1
 end
 
-html.sub!(/TABLE DATA/) { table }
+data2 = []
+for nz in data
+    data2 << "{ \"i\":#{nz['i']},\"j\":#{nz['j']},\"val\":#{nz['val']}}"
+end
+
+data_str = "var data = { \"data\":[" + data2.join(",") + "] }"
+
+data_fh = File.new(jsdatafile,"w")
+data_fh.puts data_str
 
 html.gsub!(/CELLSIZE/) { cellsize }
+html.gsub!(/HEIGHT/) { y }
+html.gsub!(/WIDTH/) { x }
 
 puts html
--- prototype/javascript.js
+++ prototype/javascript.js
@@ -2,8 +2,29 @@ var validDivs = new Object();
 validDivs['blue'] = true;
 validDivs['white'] = true;
 
+var outW;
+var outH;
+var outTop;
+var outLeft;
+
+var horDivProps = {
+    style: ""
+};
+var vertDivProps = {
+    style: ""
+};
+
 function load() {
-    var container = document.getElementById('container');
+    var outerContainer = document.getElementById('outer_container');
+    outW = outerContainer.clientWidth;
+    outH = outerContainer.clientHeight;
+    [outLeft,outTop] = getPos(outerContainer);
+
+    var container = document.getElementById('inner_container');
+    // add the drag event
+    container.addEventListener('click',clickDrag,false);
+    container.addEventListener('mouseover',initCrosshairs,false);
+    container.addEventListener('mouseout',removeCrosshairs,false);
     var containerStyle = "height:" + matrixHeight * cellsize + ";width:" + matrixWidth * cellsize+";";
     container.setAttribute('style',containerStyle);
     for ( var k=0; k<data.data.length; k++ ) {
@@ -32,4 +53,79 @@ function displayValue(event) {
     var valueDiv = document.getElementById("value");
     var str = "i = " + this.getAttribute("i") + ", j = " + this.getAttribute("j") + " => " + this.getAttribute("value");
     valueDiv.innerHTML = str;
+}
+
+function clickDrag(event) {
+    
+}
+
+function initCrosshairs(event) {
+    var target = event.target;
+    var horDiv = document.createElement('div');
+    horDiv.setAttribute("id","horDiv");
+    var width = outW;
+    horDiv.setAttribute("style","width:" + width +"px;");
+    horDivProps.style = horDiv.getAttribute("style");
+    var vertDiv = document.createElement('div');
+    vertDiv.setAttribute("id","vertDiv");
+    var height = outH;
+    vertDiv.setAttribute("style","height:" + height +"px;");
+    vertDivProps.style = vertDiv.getAttribute("style");
+    var outer = document.getElementById('outer_container');
+    outer.appendChild(horDiv)
+    outer.appendChild(vertDiv);
+    target.onmousemove = moveCrosshair;
+}
+
+function moveCrosshair(event) {
+    var cursor = getCursorPosition(event);
+    var testDiv = document.getElementById('test');
+    var x = cursor.x - outLeft;
+    var y = cursor.y - outTop;
+    testDiv.innerHTML = "x: " + x + ", y: " + y;
+    // move crosshair divs
+    var horDiv = document.getElementById('horDiv');
+    var vertDiv = document.getElementById('vertDiv');
+    horDiv.setAttribute("style",horDivProps.style + "top:" + y + ";");
+    vertDiv.setAttribute("style",vertDivProps.style + "left:" + x + ";");
+}
+
+function removeCrosshairs(event) {
+    var target = event.target;
+    target.mousemove = null;
+    var outer = document.getElementById('outer_container');
+    var horDiv = document.getElementById('horDiv');
+    var vertDiv = document.getElementById('vertDiv');
+    outer.removeChild(horDiv);
+    outer.removeChild(vertDiv);
+}
+
+function getPos(obj) {
+    var curleft = curtop = 0;
+    if ( obj.offsetParent ) {
+        do {
+            curleft += obj.offsetLeft;
+            curtop += obj.offsetTop;
+        } while ( obj = obj.offsetParent );
+        return [curleft,curtop];
+    }
+}
+
+function getCursorPosition(e) {
+    var cursor = {x:0, y:0};
+    if ( e.pageX || e.pageY ) {
+        cursor.x = e.pageX;
+        cursor.y = e.pageY;
+    }
+    else {
+        cursor.x = e.clientX +
+            (document.documentElement.scrollLeft ||
+            document.body.scrollLeft) - 
+            document.documentElement.clientLeft;
+        cursor.y = e.clientY +
+            (document.documentElement.scrollTop ||
+            document.body.scrollTop) - 
+            document.documentElement.clientTop;
+    }
+    return cursor;
 }
